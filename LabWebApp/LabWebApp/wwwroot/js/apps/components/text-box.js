e0432ae05bbd970be4d129de24da34291dcab601
kanadeiar <kanadeiar@gmail.com>
2023-11-09 23:13:34 +0300

    Новые эксперименты

---
+++ LabWebApp/LabWebApp/wwwroot/js/apps/components/text-box.js
@@ -0,0 +1,13 @@
+﻿export const TextBox = {
+    data() {
+        return {
+            text: 'text'
+        };
+    },
+    template: `
+    <div>
+        <p>Введенный текст: {{ text }}</p>
+        <input v-model="text" type="text" class="form-control" /> 
+    </div>
+    `
+}
---
+++ LabWebApp/LabWebApp/wwwroot/js/apps/firstapp.js
@@ -0,0 +1,19 @@
+﻿import { TextBox } from './components/text-box.js'
+
+const firstapp = Vue.createApp({
+    components: {
+        'text-box' : TextBox
+    },
+    data() {
+        return {
+            message: 'нажмите кнопку'
+        }
+    },
+    methods: {
+        setMessage() {
+            this.message = 'Новое сообщение приветственное!';
+        }
+    }
+});
+
+firstapp.mount("#firstapp")
