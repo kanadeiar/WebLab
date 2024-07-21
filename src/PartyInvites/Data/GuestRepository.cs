fc5a742c7ef9e5ac2d4aa8a9444d24ff5b514b71
kanadeiar <kanadeiar@gmail.com>
2024-07-21 09:50:36 +0300

    Приложение из книги

---
+++ src/PartyInvites/Data/GuestRepository.cs
@@ -0,0 +1,17 @@
+﻿using PartyInvites.Models;
+
+namespace PartyInvites.Data;
+
+public class GuestRepository
+{
+    private static List<GuestResponse> _responses = new();
+
+    public static IEnumerable<GuestResponse> Responses => _responses;
+
+    public static void AddResponse(GuestResponse response)
+    {
+        Console.WriteLine(response);
+
+        _responses.Add(response);
+    }
+}
---
+++ src/PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models;
+
+public class GuestResponse
+{
+    [Required(ErrorMessage = "Введите ваше имя")]
+    public string? Name { get; set; }
+
+    [Required(ErrorMessage = "Введите ваш адрес электронной почты")]
+    [EmailAddress]
+    public string? Email { get; set; }
+
+    [Required(ErrorMessage = "Пожалуйста, введите ваш телефон")]
+    public string? Phone { get; set; }
+
+    [Required(ErrorMessage = "Пожалйста, определитесь, будете ли вы участвовать")]
+    public bool? WillAttend { get; set; }
+}
