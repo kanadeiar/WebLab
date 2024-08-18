345c176abee57d8737a3a39643442d0fd414cbae
kanadeiar <kanadeiar@gmail.com>
2024-08-19 00:13:30 +0300

    Рефакторинг

--- src/DevelopersClub/Controllers/MeetingController.cs
+++ src/DevelopersClub/Controllers/MeetingController.cs
@@ -1,4 +1,6 @@
 ﻿using DevelopersClub.Data;
+using DevelopersClub.Web.Models;
+using Htmx;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DevelopersClub.Controllers;
@@ -7,23 +9,22 @@ public class MeetingController : Controller
 {
     public IActionResult Index(int id)
     {
-        var models = DevelopersRepository.All.ToArray();
-        var developer = DevelopersRepository.GetById(id);
-        if (developer is null) return NotFound();
+        var all = DevelopersRepository.All.ToArray();
+        var current = DevelopersRepository.GetById(id);
+        if (current is null) return NotFound();
+
+        var model = new MeetingWebModel
+        {
+            Id = id,
+            Current = current,
+            All = all,
+        };
+
+        if (Request.IsHtmx())
+        {
+            return PartialView("Partial/MeetingPartial", model);
+        }
         
-        ViewBag.Id = id;
-        ViewBag.Name = developer.Name ?? string.Empty;
-        return View(models);
-    }
-
-    public IActionResult MeetingPartial(int id)
-    {
-        var models = DevelopersRepository.All.ToArray();
-        var developer = DevelopersRepository.GetById(id);
-        if (developer is null) return NotFound();
-
-        ViewBag.Id = id;
-        ViewBag.Name = developer.Name ?? string.Empty;
-        return PartialView("Partial/MeetingPartial", models);
+        return View(model);
     }
 }
---
+++ src/DevelopersClub/Web/Models/MeetingWebModel.cs
@@ -0,0 +1,12 @@
+﻿using DevelopersClub.Models;
+
+namespace DevelopersClub.Web.Models;
+
+public class MeetingWebModel
+{
+    public int Id { get; init; }
+    
+    public required Developer Current { get; init; }
+
+    public IEnumerable<Developer> All { get; init; } = Array.Empty<Developer>();
+}
