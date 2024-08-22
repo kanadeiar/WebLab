ffb54739aa940c01731496441e956bf4fde016f1
kanadeiar <kanadeiar@gmail.com>
2024-08-22 23:48:15 +0300

    Базовая валидация

--- src/ProgrammersClub/Controllers/HomeController.cs
+++ src/ProgrammersClub/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using ProgrammersClub.Data;
-using ProgrammersClub.Models;
+using ProgrammersClub.Web.Models;
 
 namespace ProgrammersClub.Controllers;
 
@@ -12,8 +12,11 @@ public class HomeController : Controller
     }
 
     [HttpPost]
-    public IActionResult Registration(Member member)
+    public IActionResult Registration(RegistrationWebModel model)
     {
+        if (!ModelState.IsValid) return View("Index", model);
+
+        var member = model.Map();
         var id = MembersRepository.Add(member);
         return RedirectToAction("Index", "Club", new { id });
     }
---
+++ src/ProgrammersClub/Web/Models/RegistrationWebModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ProgrammersClub.Models;
+
+namespace ProgrammersClub.Web.Models;
+
+public class RegistrationWebModel
+{
+    [Required(ErrorMessage = "Пожалуйста, введите ваше имя")]
+    [StringLength(30, ErrorMessage = "Имя должно быть короче 30 символов")]
+    public string? Name { get; set; }
+
+    public Member Map()
+    {
+        return new Member
+        {
+            Name = Name,
+        };
+    }
+}
