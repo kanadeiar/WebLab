a3e22eb58ab0df05d565e34408f2bc71437d65d3
kanadeiar <kanadeiar@gmail.com>
2024-09-04 08:26:50 +0300

    Страница с правилами игры

--- src/SpyOnlineGame/Controllers/HomeController.cs
+++ src/SpyOnlineGame/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace SpyOnlineGame.Controllers
 {
@@ -13,17 +9,8 @@ namespace SpyOnlineGame.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Rules()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
