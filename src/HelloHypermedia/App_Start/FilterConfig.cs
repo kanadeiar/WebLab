53e644f76ed9a9f42c2a1318a6c38e6e49be77b8
kanadeiar <kanadeiar@gmail.com>
2024-09-18 16:17:17 +0300

    Пробное приложение ++

---
+++ src/HelloHypermedia/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace HelloHypermedia
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ src/HelloHypermedia/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HelloHypermedia.MvcApplication" Language="C#" %>
