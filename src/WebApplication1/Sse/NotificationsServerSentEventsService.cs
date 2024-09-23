4ec6d8240d6e4b9731ee3c4c13eac6c42dade375
kanadeiar <kanadeiar@gmail.com>
2024-09-23 20:23:06 +0300

    Пробное

---
+++ src/WebApplication1/Sse/NotificationsServerSentEventsService.cs
@@ -0,0 +1,15 @@
+﻿using Lib.AspNetCore.ServerSentEvents;
+using Microsoft.Extensions.Options;
+
+namespace WebApplication1.Sse
+{
+    public interface INotificationsServerSentEventsService : IServerSentEventsService
+    { }
+
+    internal class NotificationsServerSentEventsService : ServerSentEventsService, INotificationsServerSentEventsService
+    {
+        public NotificationsServerSentEventsService(IOptions<ServerSentEventsServiceOptions<NotificationsServerSentEventsService>> options)
+            : base(options.ToBaseServerSentEventsServiceOptions())
+        { }
+    }
+}
