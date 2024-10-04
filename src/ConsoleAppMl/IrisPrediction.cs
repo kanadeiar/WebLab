27d9f9008ee855c26fc499ff9bd16479ef304583
kanadeiar <kanadeiar@gmail.com>
2024-10-04 17:35:06 +0300

    Просто пробный проект

---
+++ src/ConsoleAppMl/IrisPrediction.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.ML.Data;
+
+public class IrisPrediction
+{
+    [ColumnName("PredictedLabel")]
+    public string PredictedLabel;
+}
---
+++ src/ConsoleAppMl/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.ML;
+
+Console.WriteLine("Тестирование ML");
+
+var context = new MLContext();
+var reader = context.Data.CreateTextLoader<IrisData>(separatorChar: ',', hasHeader: true);
+var dataView = reader.Load("iris.data.txt");
+
+var pipeline = context.Transforms.Conversion.MapValueToKey(outputColumnName: "Label")
+    .Append(context.Transforms.Concatenate("Features", "SepalLength", "SepalWidth", "PetalLength", "PetalWidth"))
+    .Append(context.MulticlassClassification.Trainers.SdcaMaximumEntropy("Label", "Features"))
+    .Append(context.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+
+var trainedModel = pipeline.Fit(dataView);
+
+var engine = context.Model.CreatePredictionEngine<IrisData, IrisPrediction>(trainedModel);
+
+var test = new IrisData()
+{
+    SepalLength = 3.3f,
+    SepalWidth = 1.6f,
+    PetalLength = 0.2f,
+    PetalWidth = 5.1f,
+};
+
+var prediction = engine.Predict(test);
+
+Console.WriteLine($"Тип цветка: {prediction.PredictedLabel}");
+
+Console.WriteLine("Нажмите любую кнопку ...");
+Console.ReadKey();
+
+
